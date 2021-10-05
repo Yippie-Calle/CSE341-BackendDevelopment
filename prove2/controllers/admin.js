91a3798cf1ad17e72f607cb881287ed9f9c34898
Yippie-Calle <cal12001@byui.edu>
2021-10-05 08:15:57 -0600

    tonight i finish the cart

--- prove2/controllers/admin.js
+++ prove2/controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };
@@ -25,7 +25,7 @@ exports.getEditProduct = (req, res, next) => {
     }
     const prodId = req.params.productId;
     Product.findById(prodId, product => {
-        if(!product){
+        if (!product) {
             return res.redirect('/');
         }
         res.render('admin/edit-product', {
@@ -35,6 +35,17 @@ exports.getEditProduct = (req, res, next) => {
             product: product
         });
     });
+};
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+    const updateProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
+    updateProduct.save();
+    res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
