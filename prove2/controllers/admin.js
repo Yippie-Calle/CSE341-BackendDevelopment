6a553c0efc278f7914cc94c00ac1fc1a6a3a8c59
Yippie_Calle <cal12001@byui.edu>
2021-10-04 22:46:01 -0600

    with tomorrows work I should be all caught up

--- prove2/controllers/admin.js
+++ prove2/controllers/admin.js
@@ -1,13 +1,13 @@
 const Product = require('../models/products');
+
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
+        editing: false
     })
 };
+
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
@@ -16,6 +16,25 @@ exports.postAddProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if(!product){
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    });
 };
 
 exports.getProducts = (req, res, next) => {
