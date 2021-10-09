8d3da6ac95a75a5636bec4cfc48d33f58b6b2166
Yippie_Calle <cal12001@byui.edu>
2021-10-09 13:25:56 -0600

    still cannot mongodb to populate anything yet

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+
+const app = express();
+
 const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
-const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,12 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
+  // User.findById('5baa2528563f16379fc8a610')
+  //   .then(user => {
+  //     req.user = new User(user.name, user.email, user.cart, user._id);
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
 });
 
 // app.use('/admin', adminRoutes);
